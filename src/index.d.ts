53e7d8e20e6b871a0312a20442ddc8c1c46720ec
Nikola Kretschmer <nikokre@gmail.com>
2023-04-20 19:55:51 +0200

    added Zustand State Manager

---
+++ src/index.d.ts
@@ -0,0 +1,17 @@
+export interface Podcast {
+    podcast_id: number;
+    title: string;
+    description: string;
+    image: string;
+    weekday: string;
+    category: string[];
+}
+
+export interface GlobalState {
+    selected: {
+        categories: string[],
+        podcasts: Podcast[],
+    },
+    toggleCategory: (cat: string) => void,
+    togglePodcast: (pod: Podcast) => void,
+}
---
+++ src/state.ts
@@ -0,0 +1,26 @@
+import {create} from 'zustand';
+import {GlobalState, Podcast} from "./index";
+import {devtools} from "zustand/middleware";
+
+export const useGlobal = create<GlobalState>()(devtools((set) => ({
+    selected: {
+        categories: [],
+        podcasts: [],
+    },
+    toggleCategory: (cat: string) => set(
+        (state) => {
+            if (state.selected.categories.includes(cat)) {
+                const newCats = state.selected.categories.filter((c) => c !== cat);
+                return {selected: {...state.selected, categories: newCats}}
+            }
+            return {selected: {...state.selected, categories: [...state.selected.categories, cat]}}
+        }),
+    togglePodcast: (pod: Podcast) => set(
+        (state) => {
+            if (state.selected.podcasts.includes(pod)) {
+                const newPods = state.selected.podcasts.filter((p) => p !== pod);
+                return {selected: {...state.selected, podcasts: newPods}}
+            }
+            return {selected: {...state.selected, podcasts: [...state.selected.podcasts, pod]}}
+        }),
+})));
