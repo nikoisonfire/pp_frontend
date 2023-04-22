7f43cd28f811eeecae23d0131a615e7eeee90f5a
Nikola Kretschmer <nikokre@gmail.com>
2023-04-22 12:21:11 +0200

    changed to reflect new podcast shape

--- src/defaultPodcasts.ts
+++ src/defaultPodcasts.ts
@@ -1,137 +1,95 @@
-export const defaultPodcasts = [
+import {Podcast} from "./index";
+
+export const defaultPodcasts: Podcast[] = [
     {
-        "title": "The Jeff Marek Show",
-        "podcast_id": 935175,
-        "image": "https://image.simplecastcdn.com/images/066d021a-f196-4c9a-810c-8be003e71dac/f4b1f3b8-2511-4f27-b6de-d2070e576c36/3000x3000/jeffmarekshow-v1.jpg?aid=rss_feed",
-        "description": "Sportsnet insider Jeff Marek goes around the league in Sportsnet 590 The FAN’s national NHL show, airing live across the country daily from 12pm-2pm ET. Opinion, news and analysis featuring daily segments from insider Elliotte Friedman.",
-        "drops": {
-            "dropsMonday": 0.16666666666666666,
-            "dropsTuesday": 0.23333333333333334,
-            "dropsThursday": 0.16666666666666666,
-            "dropsSaturday": 0,
-            "dropsWednesday": 0.26666666666666666,
-            "dropsFriday": 0.16666666666666666,
-            "dropsSunday": 0
-        },
+        "weekday": "Monday",
+        "description": "Its Super Effective is a weekly Pokémon podcast that covers news and information on Pokémon GO, the Pokémon anime, and the video games (such as Pokemon Scarlet & Pokémon Violet, as well as, Pokémon UNITE and Masters EX). The podcast also talks about battling, Shiny Hunting, the Pokémon TCG, Pokémon merchandise and more! While the show is mostly news focused, we cover listener e-mails, and in-depth Pokémon trivia! Its Super Effective is an award-winning podcast and one of the most downloaded Pokémon-based podcasts on the web. Its Super Effective is dedicated to bringing unique, creative Pokémon content on the internet.",
+        "title": "Its Super Effective: A Pokemon Podcast",
+        "podcast_id": 476983,
+        "image": "https://image.simplecastcdn.com/images/26542098-4d7f-4d78-8eec-7b5075986b15/c15a8dac-47e9-4045-88a1-d4e0c7fdec80/3000x3000/pkmncast-swsh-albumart-01.jpg?aid=rss_feed",
         "category": [
-            "Sports",
-            "Hockey"
+            "Leisure",
+            "Games",
+            "Animation",
+            "Manga",
+            "Video Games"
         ]
     },
     {
-        "title": "Moneycontrol Podcast",
-        "podcast_id": 471941,
-        "image": "https://images.theabcdn.com/i/35219526/s=1400x1400/el=1/rt=fill.jpg",
-        "description": "The Moneycontrol Podcast is your daily source of business news, investment analysis and advice on stocks and the markets. Tune in to broaden your horizons with podcasts by journalists, experts and analysts giving you a head-start in the investment game.",
-        "drops": {
-            "dropsMonday": 0.16666666666666666,
-            "dropsTuesday": 0.16666666666666666,
-            "dropsThursday": 0.16666666666666666,
-            "dropsSaturday": 0,
-            "dropsWednesday": 0.16666666666666666,
-            "dropsFriday": 0.3333333333333333,
-            "dropsSunday": 0
-        },
-        "category": [
-            "Business"
-        ]
-    },
-    {
-        "title": "Big Screen Sports - The Sports Movie Podcast",
-        "podcast_id": 564811,
-        "image": "https://megaphone.imgix.net/podcasts/5f6d435e-c05c-11eb-b75c-e7bb50e66687/image/img-3334.jpg?ixlib=rails-4.3.1&max-w=3000&max-h=3000&fit=crop&auto=format,compress",
-        "description": "Hosted by Kyle Bandujo, Big Screen Sports is a weekly film review podcast discussing the best and worst in the sports movie universe. The on-screen sports action, big chill moments, actors that may or may not look like athletes, and everything that worked or didnt work about your favorite and least favorite sports movies.",
-        "drops": {
-            "dropsMonday": 0.8,
-            "dropsTuesday": 0,
-            "dropsThursday": 0.03333333333333333,
-            "dropsSaturday": 0,
-            "dropsWednesday": 0.1,
-            "dropsFriday": 0.03333333333333333,
-            "dropsSunday": 0.03333333333333333
-        },
-        "category": [
-            "Tv",
-            "Film"
-        ]
-    },
-    {
-        "title": "Wild Business Growth Podcast",
-        "podcast_id": 759243,
-        "image": "https://ssl-static.libsyn.com/p/assets/c/8/a/1/c8a135ba99790c33/Max_OFFICIAL_Wild_Business_Growth_Podcast.jpg",
-        "description": "Wild ideas. Wild growth. Every Wednesday morning a new entrepreneur shares the wildly creative and unusual ways they grew their business - and teaches you how to do it, too. Hear the minds behind WordPress, Stacys Pita Chips, Drybar, the voice of Bart Simpson, and more wild stories. Hosted by Max Branstetter, Founder and Podcast Producer at MaxPodcasting",
-        "drops": {
-            "dropsMonday": 0,
-            "dropsTuesday": 0,
-            "dropsThursday": 0.06666666666666667,
-            "dropsSaturday": 0,
-            "dropsWednesday": 0.8,
-            "dropsFriday": 0.13333333333333333,
-            "dropsSunday": 0
-        },
-        "category": [
-            "Business",
-            "Entrepreneurship"
-        ]
-    },
-    {
-        "title": "Today in PA | A PennLive daily news briefing with Claudia Dimuro",
-        "podcast_id": 1038496,
-        "image": "https://megaphone.imgix.net/podcasts/968c0d9e-ca3c-11ec-9c81-1fd859845e8e/image/show-cover.jpg?ixlib=rails-4.3.1&max-w=3000&max-h=3000&fit=crop&auto=format,compress",
-        "description": "Its what you need to know about whats happening in Pennsylvania, delivered to you in around 5 minutes every weekday morning by PennLive.com reporter Claudia Dimuro. Today in Pa. brings you the biggest stories in the state, whether theyre hard news, entertainment, sports or business.",
-        "drops": {
-            "dropsMonday": 0.2,
-            "dropsTuesday": 0.2,
-            "dropsThursday": 0.16666666666666666,
-            "dropsSaturday": 0,
-            "dropsWednesday": 0.2,
-            "dropsFriday": 0.23333333333333334,
-            "dropsSunday": 0
-        },
+        "weekday": "Tuesday",
+        "description": "Covering the apocalypse from the left. theinsurgents.substack.com",
+        "title": "The Insurgents",
+        "podcast_id": 439136,
+        "image": "https://substackcdn.com/feed/podcast/28705/ff72a11ca939f5604f17d4a689071b23.jpg",
         "category": [
             "News",
-            "Daily"
-        ]
-    },
-    {
-        "title": "Air Tight / In My Grow Show",
-        "podcast_id": 940646,
-        "image": "https://ssl-static.libsyn.com/p/assets/8/d/7/2/8d72074e4be5e385d959afa2a1bf1c87/EyeBall_Candle_Burst_Red_3.png",
-        "description": "Air Tight is a cannabis infused music show hosted by The Alex (a.k.a. Alex Robles).  Were going to play some music from the archives of Green Coast Radio, read some headlines.  So roll up a joint or heat up your rig and hang out.",
-        "drops": {
-            "dropsMonday": 0.1,
-            "dropsTuesday": 0.23333333333333334,
-            "dropsThursday": 0.13333333333333333,
-            "dropsSaturday": 0.06666666666666667,
-            "dropsWednesday": 0.03333333333333333,
-            "dropsFriday": 0.1,
-            "dropsSunday": 0.3333333333333333
-        },
-        "category": [
-            "Music",
-            "News",
+            "Politics",
             "Commentary"
         ]
     },
     {
-        "title": "Kinda Funny Games Daily: Video Games News Podcast",
-        "podcast_id": 989909,
-        "image": "https://megaphone.imgix.net/podcasts/28c2b81a-9182-11eb-9898-2fecbec465cf/image/KFGD_Test.jpg?ixlib=rails-4.3.1&max-w=3000&max-h=3000&fit=crop&auto=format,compress",
-        "description": "The world’s most popular daily video game news podcast hosted by Greg “GameOverGreggy” Miller, Tim Gettys, Blessing Adeoye, Star Wars Rogue One writer Gary Whitta, alongside a cast of games industry veterans and guests covering the latest in PlayStation, Xbox, Nintendo and all of gaming. Watch the free video version at YouTube.com/KindaFunnyGames",
-        "drops": {
-            "dropsMonday": 0.16666666666666666,
-            "dropsTuesday": 0.2,
-            "dropsThursday": 0.2,
-            "dropsSaturday": 0,
-            "dropsWednesday": 0.23333333333333334,
-            "dropsFriday": 0.2,
-            "dropsSunday": 0
-        },
+        "weekday": "Wednesday",
+        "description": "How do I sell on Teachers Pay Teachers? How do I make money online as a teacher? How to make money online teaching? How do I grow a teacher Instagram? Why did I quit teaching and what happened next? Whether you are a teacher just looking to get started building a business online, or youre ready to be the next official CEO Teacher, this podcast is for you. \n\nThe CEO Teacher Podcast is a weekly teacher-filled workshop that will fill your ears with actionable strategies that you can take back to your home and get started on today. The CEO Teacher Podcast is hosted by Kayse Morris, creator of The CEO Teacher School Membership and The CEO Teacher Academy membership, home to over 3,000 teacher students where she trains teachers each week how to sell their products on Teachers Pay Teachers and beyond. \n\nHere youll find easy step-by-step strategies for getting more teacher sales online, building a serious social media strategy, mindset shifts, and so much more. In addition to sharing her knowledge, Kayse also interviews real teachers that have gone on to build the teacher business of their dreams. With nearly 1 million downloads, come listen and subscribe to the #1 teacher podcast in the world....and remember that the best is yet to come.",
+        "title": "The CEO Teacher Podcast",
+        "podcast_id": 487640,
+        "image": "https://ssl-static.libsyn.com/p/assets/e/1/2/9/e1299d90010da277bafc7308ab683e82/the-ceo-teacher-podcast-20220817-w3x5tjnhx0.png",
         "category": [
-            "Leisure",
-            "Games",
+            "Business",
+            "Marketing"
+        ]
+    },
+    {
+        "weekday": "Thursday",
+        "description": "Ever wondered how automation will change the world? Maybe you puzzle over what India could do to ease traffic congestion, or how Chinas aircraft carriers will transform Indian Ocean geopolitics? All Things Policy, a daily podcast brought to you by the Takshashila Institution, brings you all the answers. Every weekday, our researchers break down complex economic and geopolitical ideas through the lens of current events. For everyone from the busy executive to the curious student, All Things Policy is all youll need to understand the world (and appreciate your breakfast) better.",
+        "title": "All Things Policy",
+        "podcast_id": 220534,
+        "image": "https://www.omnycontent.com/d/playlist/e0dce4b3-2eb8-48cb-822c-af1d00e03e20/f87e16b6-f073-436b-91d1-af4d00c69ff1/90da458e-82ee-498b-b694-af4d00c69ffb/image.jpg?t=1668427393&size=Large",
+        "category": [
             "News",
-            "Video Games"
+            "Education",
+            "Society",
+            "Culture",
+            "Leisure",
+            "Automotive"
+        ]
+    },
+    {
+        "weekday": "Friday",
+        "description": "A podcast for the modern day Asian American woman hosted by Melody Cheng, Helen Wu, & Janet Wang. Its no secret that Asian women are lacking in almost all media outlets, and those who are present mostly represent the fashion, beauty, or blogging industries. We started ABG as girlfriends while balancing jobs in finance, technology, & media with corporate careers spanning over a decade. Here, we share our experiences and explore topics as 20/30 something Asian American women working, dating, and living in LA, CA. Grab a glass of wine and lets get this show started ;)",
+        "title": "AsianBossGirl",
+        "podcast_id": 495622,
+        "image": "https://megaphone.imgix.net/podcasts/072ad28a-e35a-11eb-8f11-e79e9de6eb51/image/3075001-1582092618833-2f1adbf794187.jpg?ixlib=rails-4.3.1&max-w=3000&max-h=3000&fit=crop&auto=format,compress",
+        "category": [
+            "Society",
+            "Culture",
+            "Personal",
+            "Journals",
+            "Business",
+            "Careers"
+        ]
+    },
+    {
+        "weekday": "Saturday",
+        "description": "Earth Ancients chronicles the growing (and often suppressed) evidence of known and unknown civilizations, their ruined cities, and artifacts developed from advanced science and technology. Erased from the pages of time, these cultures discovered and charted the heavens, developed medicine and unleashed advancements that parallel and, in many cases, surpass our own. Join us and discover our lost history.Armed with the thousands of anomalous archeological discoveries which have not been covered by conventional science and the media, we can no longer deny our ancient cultural inheritance. Our written history is wrong and we’ve been led to believe that humanity is just a few hundred thousand years old. In fact, the Hindu Yugas advance the notion that Homo Sapiens are millions of years old, and have lived onplanet Earth through a series of rebirths.It’s now a fact that we are the survivors of a series of cataclysmic events that took place approximately 12,000 to 14,000 years ago. Our ancestors may have been aware of these impending disasters and fled underground shelter, or survived in caves; others may have left the planet, but a huge number perished.      Though Earth Ancients does explore some of the popular theories that ancient aliens have visited ourplanet, our philosophy and research paradigm is decidedly Earth-centric, elevating the historical discourseabout human brilliance and ingenuity found in the archaeological evidence.",
+        "title": "Earth Ancients",
+        "podcast_id": 391184,
+        "image": "https://d3wo5wojvuv7l.cloudfront.net/t_rss_itunes_square_1400/images.spreaker.com/original/88d9888baa9b9f147a64a175d3fc032e.jpg",
+        "category": [
+            "Society",
+            "Culture",
+            "Science",
+            "Social"
+        ]
+    },
+    {
+        "weekday": "Sunday",
+        "description": "Tenured professor, registered dietitian nutritionist, and certified exercise physiologist Dr. Neal Malik shares some of the best articles on Health, Fitness, Diet, Nutrition, and Wellness, with commentary. Why bother searching for the best blogs about health & fitness when it can be found and read for you? Think of Optimal Health Daily as an audioblog or blogcast.",
+        "title": "Optimal Health Daily",
+        "podcast_id": 323906,
+        "image": "https://megaphone.imgix.net/podcasts/179f5c46-1733-11ec-86d8-6fde4865f964/image/OHD1500Redo.jpg?ixlib=rails-4.3.1&max-w=3000&max-h=3000&fit=crop&auto=format,compress",
+        "category": [
+            "Health",
+            "Fitness"
         ]
     }
 ];
