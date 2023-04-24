670488858a9f767ef551e1431c6496d240e44f03
Nikola Kretschmer <nikokre@gmail.com>
2023-04-24 17:18:14 +0200

    added Screensizes

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,12 +2,28 @@ const withMT = require("@material-tailwind/react/utils/withMT");
 
 /** @type {import('tailwindcss').Config} */
 module.exports = withMT({
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+    content: [
+        "./index.html",
+        "./src/**/*.{js,ts,jsx,tsx}",
+    ],
+    theme: {
+        extend: {},
+        screens: {
+            'sm': '425px',
+            // => @media (min-width: 640px) { ... }
+
+            'md': '680px',
+            // => @media (min-width: 768px) { ... }
+
+            'lg': '1024px',
+            // => @media (min-width: 1024px) { ... }
+
+            'xl': '1280px',
+            // => @media (min-width: 1280px) { ... }
+
+            '2xl': '1536px',
+            // => @media (min-width: 1536px) { ... }
+        }
+    },
+    plugins: []
 });
